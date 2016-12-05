7000e8a74963b1726ba8dcd5f7b406f4cc70f6cf
AurelienNioche <nioche.aurelien@gmail.com>
2016-12-05 18:07:03 +0100

    Various upgrades.

--- run_on_avakas.sh
+++ run_on_avakas.sh
@@ -7,7 +7,7 @@ python3 setup.py build_ext --inplace
 echo "Switch to python 2.7.12"
 pyenv local 2.7.12
 
-echo "Call 'avakas_launcher.py'"
+echo "Submit 'avakas_script.sh'"
 qsub avakas_script.sh
 
 echo "Come back to python 3.5.2"
